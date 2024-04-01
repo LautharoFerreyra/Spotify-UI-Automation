dfd097750f4557b3d15462ca83b3745c42b8d076
lautharo <lautharoferreyra@gmail.com>
2024-04-01 13:57:25 -0300

    Task TC 0001 y TC 0002

---
+++ cypress/test-data/login.js
@@ -0,0 +1,9 @@
+const loginData = {
+    validUser: "ellautha@gmail.com",
+    validPassword: "randompassword123", 
+    invalidUser: "asdasd@gmail.com",
+    invalidPassword: "bazzinga72"
+
+};
+  
+  export default loginData;
