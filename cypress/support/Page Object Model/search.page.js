11c7a7aeb7192dece406cb4374bec9753cec05d4
lautharo <lautharoferreyra@gmail.com>
2024-04-26 18:45:46 -0300

    test branch

--- cypress/support/Page Object Model/search.page.js
+++ cypress/support/Page Object Model/search.page.js
@@ -1,10 +1,10 @@
 class searchPage{ 
     
-    get searchBard(){ 
-        return cy.get(".CVuGEUIxLkNKpMds8AFS")
-    }
     get searchButtom(){
         return cy.get(".KAcp7QFuEYSouAsuC5i_")
+    }
+    get searchBard(){ 
+        return cy.get(".CVuGEUIxLkNKpMds8AFS")
     }
     
 
